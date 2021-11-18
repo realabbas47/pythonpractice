4e9c0abac925670ecd948280b06a13228abde8cb
realabbas47 <94651745+realabbas47@users.noreply.github.com>
2021-11-18 17:48:05 -0500

    Add files via upload

---
+++ Intro/Lesson1.py
@@ -0,0 +1,24 @@
+"""
+Constant
+"""
+print('\n\nThese are all constants\n')
+print(123)
+print(98.3)
+print('Hello World')
+
+"""
+Reserved words
+"""
+
+print('\n\nThese are all reserved words\n')
+print('False    Class   Return  is  finally\nNone   if  for lambda  continue\nTrue  def from    while   nonlocal\nand   del global  not with\nas    elif    try or  yield\nassert  else import  pass\nbreak except  in  raise')
+
+"""
+Variables
+"""
+
+print('\n\nThis is a variable\n')
+print('x = 12.2')
+
+print('\nRules for Variable name\n')
+
